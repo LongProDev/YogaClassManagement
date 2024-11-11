172b58f27856c93c7e512ec62b96c5a9dcd7d74d
LongProDev <thanhlongn08@gmail.com>
2024-11-11 14:34:52 +0700

    update env

--- yoga-backend/server.js
+++ yoga-backend/server.js
@@ -26,7 +26,7 @@ db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
 // Start server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3666;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
